06b419541c83241228264368b3977e0335715b20
jp0301 <jp0301@naver.com>
2022-10-31 18:14:27 +0900

    update 2022-10-31

---
+++ BoardPratice01.sql
@@ -0,0 +1,77 @@
+SELECT USER
+FROM DUAL;
+
+--○ 게시물 번호의 최대값을 얻어내는 쿼리문 구성
+SELECT NVL(MAX(NUM), 0) AS MAXNUM
+FROM TBL_BOARD;
+
+--○ 게시물 작성 쿼리문 구성
+INSERT INTO TBL_BOARD(NUM, NAME, PWD, EMAIL, SUBJECT, CONTENT, IPADDR, HITCOUNT, CREATED)
+VALUES(1, '고연수', '1234', 'soo@test.com', '제목테스트', '내용테스트', '211.238.142.60', 0, SYSDATE);
+
+--○ DB 레코드의 갯수를 가져오는 쿼리문 구성
+-- 페이징의 갯수를 결정짓기 위해 만드는 것이다.
+SELECT COUNT(*) AS COUNT
+FROM TBL_BOARD;
+
+--○ 특정 영역의(시작번호 ~ 끝번호) 게시물의 목록을 읽어오는 쿼리문 구성
+--   번호, 이름, 제목, 조회수, 작성일
+SELECT NUM, NAME, SUBJECT,HITCOUNT, CREATED
+FROM
+(
+    SELECT ROWNUM RNUM, DATA.*
+    FROM
+    (
+    SELECT NUM, NAME, SUBJECT, HITCOUNT, TO_CHAR(CREATED, 'YYYY-MM-DD') AS CREATED
+    FROM TBL_BOARD
+    ORDER BY NUM DESC
+    ) DATA
+)
+WHERE RNUM >= 1 AND RNUM<= 10;
+
+--○ 특정 게시물 조회에 따른 조회 횟수 증가 쿼리문 구성
+UPDATE TBL_BOARD
+SET HITCOUNT = HITCOUNT + 1
+WHERE NUM = 1;
+
+--○ 특정 게시물의 내용을 읽어오는 쿼리문 구성
+SELECT NUM, NAME, SUBJECT, PWD, EMAIL, CONTENT, IPADDR, HITCOUNT, TO_CHAR(CREATED, 'YYYY-MM-DD') AS CREATED
+FROM TBL_BOARD
+WHERE NUM = 1;
+
+--○ 특정 게시물을 삭제하는(제거하는) 쿼리문 구성
+DELETE
+FROM TBL_BOARD
+WHERE NUM = 1;
+
+--○ 특정 게시물을 수정하는 쿼리문 구성
+UPDATE TBL_BOARD
+SET NAME='' , PWD='', EMAIL='', SUBJECT='', CONTENT=''
+WHERE NUM=1;
+
+--○ 지금 열람하는 게시물의 이후 게시물
+SELECT NVL(MAX(NUM), -1) AS NEXTNUM
+FROM TBL_BOARD
+WHERE NUM > 500;
+
+--○ 지금 열람하는 게시물의 이전 게시물
+SELECT NVL(MIN(NUM), -1) AS BEFORENUM
+FROM TBL_BOARD
+WHERE NUM < 500;
+
+--○ 게시물 검색 기능 관련 쿼리문 구성
+SELECT COUNT(*) AS COUNT
+FROM TBL_BOARD
+WHERE SUBJECT LIKE '%날씨%';
+
+SELECT COUNT(*) AS COUNT
+FROM TBL_BOARD
+WHERE CONTENT LIKE '%음식%';
+
+SELECT COUNT(*) AS COUNT
+FROM TBL_BOARD
+WHERE NAME LIKE '%동현%';
+
+
+
+
